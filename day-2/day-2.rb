67fec558a7fb3499020cc350f2864e057a635cb2
michaelegregious <mbush@greenkeytech.com>
2021-12-02 07:23:36 -0600

    wip

--- day-2/day-2.rb
+++ day-2/day-2.rb
@@ -16,5 +16,26 @@ def auto_pilot(directives)
   x * depth
 end
 
+# part 2
+def auto_pilot(directives)
+  x = depth = aim = 0
+  directives.each do |directive|
+    dir, n = directive.split(' ')
+    units = Integer(n)
+    case dir
+    # when 'forward' then x += units
+    when forward
+      x += units
+      depth += aim * units
+
+    # when 'down' then depth += units
+    when 'down' then aim += units
+    # when 'up' then depth -= units
+    when 'up' then aim -= units
+    end
+  end
+  x * depth
+end
+
 p auto_pilot(directives)
 
