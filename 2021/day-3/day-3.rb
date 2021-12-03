ca189ee019cb3c3674074a127318572b4f060ce6
michaelegregious <mbush@greenkeytech.com>
2021-12-03 17:16:15 -0600

    pt 2 wip

--- 2021/day-3/day-3.rb
+++ 2021/day-3/day-3.rb
@@ -17,10 +17,9 @@ test_data = %w(
   01010
 )
 
-def run_diagnostics(binary)
-  # tally: [ 0 count, 1 count ] per index
-  tally = []
-  x = 0
+# part 1
+def gamma_epsilon_diagnostics(binary)
+  tally = [] # [ count 0, count 1 ] per index
 
   binary.each do |str|
     str.chomp.chars.each_with_index do |char, i|
@@ -46,4 +45,89 @@ def run_diagnostics(binary)
   gamma * epsilon
 end
 
-p run_diagnostics(test_data)
+# consider first bit of data
+# keep only Ns of the bit criteria
+# if only one number left, stop, that's it
+# else repeat the process, moving one column to the right
+
+# bit criteria:
+# oxygen gen rating:
+#  - consider most common val, 0 or 1
+#  - if equally common, keep values with 1
+#  CO2 rating:
+#  - least common value, 0 or 1
+#  - if equally common, keep 0
+
+# part 2
+class LifeSupportDiagnostics
+  def initialize(data = BINARY)
+    @data = data
+  end
+
+  def oxygen_generator_rating(data = @data, selected_index = 0)
+    @pairs ||= gamma_epsilon_pairs(data)
+    @most_common = most_common_per_index(pairs)
+    selected = []
+
+    data.each_with_index do |str, i|
+      if str.chomp[selected_index] == most_common[selected_index]
+        selected << str
+      end
+    end
+
+    if selected_indices.size == 1
+      return selected[0]
+    else
+      oxygen_generator_rating(selected, selected_index + 1)
+    end
+  end
+
+
+
+
+
+  private
+
+  def most_common_per_index(pairs)
+    pairs.map{ |pair| pair[0] > pair[1] ? 0 : 1 }
+  end
+
+  def gamma_epsilon_pairs(data)
+    tally = [] # [ count 0, count 1 ] per index
+
+    binary.each do |str|
+      str.chomp.chars.each_with_index do |char, i|
+        tally[i] ||= [0, 0]
+        if char == '0' then tally[i][0] += 1
+        elsif char == '1' then tally[i][1] += 1
+        end
+      end
+    end
+
+    tally
+  end
+end
+
+
+
+p gamma_epsilon_pairs(test_data)
+# p run_diagnostics(test_data)
+
+
+# def CO2_scrubber_rating(data = @data, selected_index = 0)
+#   @pairs ||= gamma_epsilon_pairs(data)
+#   @most_common = most_common_per_index(pairs)
+#   selected = []
+
+#   data.each_with_index do |str, i|
+#     if str.chomp[selected_index] != most_common[selected_index]
+#       selected << str
+#     end
+#   end
+
+#   if selected_indices.size == 1
+#     return selected[0]
+#   else
+#     C02_rating(selected, selected_index + 1)
+#   end
+# end
