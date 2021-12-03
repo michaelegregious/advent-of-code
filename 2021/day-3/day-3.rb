a9f8818df64febc82a36c105381b4c70a340fc66
michaelegregious <mbush@greenkeytech.com>
2021-12-03 14:55:01 -0600

    pt 1 working

---
+++ 2021/day-3/day-3.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+# directives = File.open('./data.txt').readlines
+
+test_data = %w(
+  00100
+  11110
+  10110
+  10111
+  10101
+  01111
+  00111
+  11100
+  10000
+  11001
+  00010
+  01010
+)
+
+def run_diagnostics(bin)
+  # tally: [ 0 count, 1 count ] per index
+  tally = []
+
+  bin.each do |str|
+    str.chars.each_with_index do |char, i|
+      tally[i] ||= [0, 0]
+      if char == '0'
+        tally[i][0] += 1
+      elsif char == '1'
+        tally[i][1] += 1
+      end
+    end
+  end
+
+  gamma = tally
+    .map { |pair| pair[0] > pair[1] ? 0 : 1 }
+    .join
+    .to_i(2)
+
+  epsilon = tally
+    .map { |pair| pair[0] < pair[1] ? 0 : 1 }
+    .join
+    .to_i(2)
+
+  gamma * epsilon
+end
+
+p run_diagnostics(test_data)
