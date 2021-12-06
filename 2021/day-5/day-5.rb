05960f91cbae0fb8f249bb775466ab44d8566ea4
michaelegregious <mbush@greenkeytech.com>
2021-12-06 12:03:00 -0600

    correct solution for pt 2

--- 2021/day-5/day-5.rb
+++ 2021/day-5/day-5.rb
@@ -27,29 +27,32 @@ def decipher_vents(ranges)
   count = {co: 0}
   ranges = ranges.each_with_object([]) do |coords, field|
     start, fin = coords
+    (y1, x1), (y2, x2) = start, fin
     if start[1] == fin[1]
-      set_locations(field, [start[0], fin[0]], start[1], count)
+      set_locations(field, [y1, y2], x1, count)
     elsif start[0] == fin[0]
-      set_locations(field, [start[1], fin[1]], start[0], count, true)
+      set_locations(field, [x1, x2], y1, count, true)
+    else
+      x_range = (x1 < x2 ? (x1..x2) : x1.downto(x2)).to_a
+      y_range = (y1 < y2 ? (y1..y2) : y1.downto(y2)).to_a
+      set_diagonals(field, x_range, y_range, count)
     end
   end
   [ranges, count]
 end
 
-
-
 def set_locations(field, (x1, x2), y, count, y_mode = false)
   x = y if y_mode
   range = (x1..x2).size == 0 ? (x2..x1) : (x1..x2)
   if y_mode
-    range.each_with_index do |y|
+    range.each do |y|
       field[y] ||= []
       c = (field[y][x] || 0) + 1
       field[y][x] = c
       count[:co] += 1 if c == 2
     end
   else
-    range.each_with_index do |x|
+    range.each do |x|
       field[y] ||= []
       c = (field[y][x] || 0) + 1
       field[y][x] = c
@@ -58,6 +61,14 @@ def set_locations(field, (x1, x2), y, count, y_mode = false)
   end
 end
 
+def set_diagonals(field, y_range, x_range, count)
+  y_range.each_with_index do |y, i|
+    field[y] ||= []
+    c = (field[y][x_range[i]] || 0) + 1
+    field[y][x_range[i]] = c
+    count[:co] += 1 if c == 2
+  end
+end
 
 
 ranges = make_ranges(real_data)
