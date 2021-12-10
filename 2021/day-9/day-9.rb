61f1560ac14412831e7fe5730aa886bbf60c1748
michaelegregious <mbush@greenkeytech.com>
2021-12-10 09:00:04 -0600

    day-9 pt1 working (finally cleaned up the input properly)

--- 2021/day-9/day-9.rb
+++ 2021/day-9/day-9.rb
@@ -13,25 +13,38 @@ test_data = [
 ]
 
 def process_data(data)
-  data.map(&:to_i)
+  data.map{ |line| line.chomp.split('').map(&:to_i)}
 end
 
 def find_low_pts(rows)
   low_pts = []
 
   rows.each_with_index do |row, i|
-    row.chars.each_with_index do |num, j|
+    row.each_with_index do |num, j|
       up = i > 0 ? rows[i - 1][j] : nil
       right = j < row.length - 1 ? row[j + 1] : nil
       left = j == 0 ? nil : row[j - 1]
       down = i < rows.length - 1 ? rows[i + 1][j] : nil
 
-      if [up, down, left, right].compact.all? { |adj| num < adj }
-        low_pts << num
+      adjacents = [up, down, left, right].compact
+
+      if j == row.length - 1 && i == 0
+        # p row.length - 1
+        # p num
+        # p adjacents
+        p "up: #{up}"
+        p "down: #{down}"
+        p "left: #{left}"
+        p "right: #{right}"
+      end
+
+      if adjacents.all? { |adj| num < adj }
+        low_pts << num + 1
       end
     end
   end
-  low_pts
+  low_pts.sum
 end
 
-p find_low_pts(test_data)
+data = process_data(real_data)
+p find_low_pts(data)
