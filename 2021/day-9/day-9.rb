853ecb79696a859bb46159104f595caf9773c7b9
michaelegregious <mbush@greenkeytech.com>
2021-12-10 18:56:24 -0600

    wip

--- 2021/day-9/day-9.rb
+++ 2021/day-9/day-9.rb
@@ -16,9 +16,9 @@ def process_data(data)
   data.map{ |line| line.chomp.split('').map(&:to_i)}
 end
 
+# part 1
 def find_low_pts(rows)
   low_pts = []
-
   rows.each_with_index do |row, i|
     row.each_with_index do |num, j|
       up = i > 0 ? rows[i - 1][j] : nil
@@ -36,5 +36,60 @@ def find_low_pts(rows)
   low_pts.sum
 end
 
-data = process_data(real_data)
-p find_low_pts(data)
+# data = process_data(real_data)
+# p find_low_pts(data)
+
+# part 2
+def low_pts(rows)
+  low_pts = []
+  rows.each_with_index do |row, i|
+    row.each_with_index do |num, j|
+      up = i > 0 ? rows[i - 1][j] : nil
+      right = j < row.length - 1 ? row[j + 1] : nil
+      left = j == 0 ? nil : row[j - 1]
+      down = i < rows.length - 1 ? rows[i + 1][j] : nil
+
+      adjacents = [up, down, left, right].compact
+
+      if adjacents.all? { |adj| num < adj }
+        low_pts << [i, j]
+      end
+    end
+  end
+  low_pts
+end
+
+def basin(low_pt, rows)
+  x, y = low_pt
+  basin_pts = []
+  adjacents = [
+    [x - 1, y],
+    [x, y + 1],
+    [x + 1, y],
+    [x, y - 1],
+  ]
+
+  adjacents.each do |(i, j)|
+    if has_n?([i, j], rows) && rows[i][j] > rows[x][y] && rows[i][j] < 9
+      greater_adjacents([i, j], rows).each { |pr| basin_pts << pr }
+    end
+  end
+
+  return basin_pts.unshift(low_pt)
+end
+
+def has_n?((i, j), rows)
+  i, j = pt
+  return false if !i&.between?(0, rows.length - 1) || !j&.between?(0, rows[0].length - 1)
+  return true
+end
+
+def largest_basins(rows)
+  low_pts(rows).reduce([]) do |sizes, low_pt|
+    basins << low_pt
+  end.sort.take(3).reduce(:*)
+end
+
+data = process_data(test_data)
+# p largest_basins(data)
+p basin([4, 6], data).uniq
