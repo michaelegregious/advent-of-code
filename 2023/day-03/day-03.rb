85445e6d79e7ce212ab92859ceb2c3d4ed441aa4
michaelegregious <lil.gordie@gmail.com>
2023-12-22 13:16:29 -0600

    find_gears working (part 2)

--- 2023/day-03/day-03.rb
+++ 2023/day-03/day-03.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 real_data = File.open('./data.txt').readlines
 
 test_data = %w(
@@ -36,10 +38,6 @@ class GondolaLift
     end
   end
 
-  def is_numeric?(obj)
-    obj.to_s.match(/\d+/) == nil ? false : true
-  end
-
   def find_part_numbers
     @coordinates.each_with_object([]) do |((x, y), value), part_numbers|
       catch :part_number_found do
@@ -49,7 +47,7 @@ class GondolaLift
 
           number_length.times do |num|
             adjacencies(x + num, y).each do |adjacency|
-              if (@coordinates[adjacency] && !is_numeric?(@coordinates[adjacency]))
+              if @coordinates[adjacency] && !is_numeric?(@coordinates[adjacency])
                 part_numbers << value.to_i
 
                 throw :part_number_found
@@ -58,6 +56,43 @@ class GondolaLift
           end
         end
       end
+    end
+  end
+
+  def find_gears
+    # p "coordinates: #{@coordinates}"
+    p "plumped_up_number_coordinates: #{plumped_up_number_coordinates}"
+    @coordinates.each_with_object([]) do |((x, y), value), verified_gears|
+      if value == '*'
+        adjacent_part_numbers = Set.new([])
+
+        adjacencies(x, y).each do |adjacency|
+          # p "adjacency: #{adjacency}"
+          # p "@plumped_up_number_coordinates[adjacency]: #{plumped_up_number_coordinates[adjacency]}"
+          # p "@coordinates[@plumped_up_number_coordinates[adjacency]]: #{@coordinates[@plumped_up_number_coordinates[adjacency]]}"
+
+          if @plumped_up_number_coordinates[adjacency] &&
+            is_numeric?(@coordinates[@plumped_up_number_coordinates[adjacency]])
+
+            adjacent_part_numbers.add(@coordinates[@plumped_up_number_coordinates[adjacency]].to_i)
+          end
+        end
+        p "adjacent_part_numbers: #{adjacent_part_numbers}"
+
+        verified_gears << adjacent_part_numbers if adjacent_part_numbers.length == 2
+        p "verified_gears: #{verified_gears}"
+      end
+    end
+  end
+
+  private
+
+  # returns a hash of [x, y] => [x, y] coordinates for each digit pointing to the coordinate of the first digit
+  def plumped_up_number_coordinates
+    @plumped_up_number_coordinates ||= @coordinates.each_with_object({}) do |((x, y), digit), plumped_up|
+      next unless is_numeric?(digit)
+
+      digit.length.times { |idx| plumped_up[[x + idx, y]] = [x, y] }
     end
   end
 
@@ -74,7 +109,15 @@ class GondolaLift
     ]
   end
 
+  def is_numeric?(obj)
+    obj.to_s.match(/\d+/) == nil ? false : true
+  end
+
 end
 
-p GondolaLift.new(test_data).find_part_numbers.sum
+# part 1
+# p GondolaLift.new(test_data).find_part_numbers.sum
+# p GondolaLift.new(process_data(real_data)).find_part_numbers.sum
 
+# part 2
+p GondolaLift.new(test_data).find_gears
